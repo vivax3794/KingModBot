b7a5e99b59633edc1f84b574bab49670e1487ac3
derekolson09 <d.g.olson09@gmail.com>
2019-12-29 04:50:21 -0800

    update(code): Cleanup of extraneous code

--- src/KingModBot/models/TW_Connect_Mod.py
+++ src/KingModBot/models/TW_Connect_Mod.py
@@ -15,15 +15,12 @@ class TW_Connect():
     """
 
     def __init__(self, path_to_secrets):
-        self.client_id = ''
-        self.token = ''
         self.path_to_secrets = pathlib.Path(path_to_secrets)
 
     def hash_connection(self):
         # Handles the hashing of the connection information so that it is not identifiable in a print statement
         secrets = self.read_secrets()
-        self.client_id = secrets.twitch_con.client_id
-        self.token = secrets.twitch_con.token
+
         connection = []
         hash_client_id = hashlib.sha512(
             str(secrets.twitch_con.client_id).encode('utf-8'))
@@ -37,4 +34,6 @@ class TW_Connect():
         with open((self.path_to_secrets.absolute()), "r") as json_data:
             data = json_data.read()
             x = json.loads(data, object_hook=lambda d: Namespace(**d))
+            self.client_id = x.twitch_con.client_id
+            self.token = x.twitch_con.token
             return x
