99cb449347643d7fa320fbeeaf5f710db37f8078
derekolson09 <d.g.olson09@gmail.com>
2019-12-29 04:11:12 -0800

    update(remove): got rid of extraneous code for test setup

--- src/KingModBot/scripts/main.py
+++ src/KingModBot/scripts/main.py
@@ -7,8 +7,7 @@ import sys
 def main():
     # Get the path to the secrets folder
     secrets_path = sys.argv[1]
-
     twitch_connection = TW_Connect(secrets_path)
-
-    print(twitch_connection.hash_connection())
-    print(twitch_connection.read_secrets())
+    # Hash connections/api tokens for printable views
+    # Raw view will be on the self.client_id and self.token
+    twitch_connection.hash_connection()
